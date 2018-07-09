d3f1bb78f583d7004e9d0cbebb78c6f8ad57731b
Zach Chrystall <z.chrystall@gmail.com>
2018-07-09 10:00:18 +1000

    getting data from reddit

--- App.js
+++ App.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = { data: { earthPornData: [], peopleDyingData: [] }}
+  }
+
+  componentDidMount() {
+    const earthPornData = fetch('https://www.reddit.com/r/earthporn.json')
+      .then((response) => response.json())
+      .then((responseJson) => responseJson.data.children.splice(0, 10))
+      .catch((error) => {
+        console.error(error);
+      });
+
+    const peopleDyingData = fetch('https://www.reddit.com/r/peopleFuckingDying.json')
+      .then((response) => response.json())
+      .then((responseJson) => responseJson.data.children.splice(0, 10))
+      .catch((error) => {
+        console.error(error);
+      });
+
+    Promise.all([earthPornData, peopleDyingData])
+      .then(([earthPornData, peopleDyingData]) => this.setState({
+        data: {
+          earthPornData,
+          peopleDyingData,
+        }
+      }));
+  }
+
   render() {
+    const { data } = this.state;
+
+    console.log(data.earthPornData.length);
+    console.log('=================================');
+    console.log(data.peopleDyingData.length);
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
@@ -21,3 +58,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
